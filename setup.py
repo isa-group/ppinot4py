aea2e2a401c64710a0636b571a580cc7b83ce147
Manuel Resinas <resinas@us.es>
2021-02-22 17:07:27 +0100

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ppinot4py", 
-    version="1.1.1",
+    version="1.1.2",
     author="ISA Group",
     author_email="resinas@us.es",
     description="PPINOT for Python (ppinot4py)",
