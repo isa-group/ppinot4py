de0cc2867ba1ab1a02dc1750f7fb43d9dc046790
Manuel Resinas <resinas@us.es>
2024-04-02 11:41:58 +0200

    Updates version to 1.2.1 for release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ppinot4py", 
-    version="1.2",
+    version="1.2.1",
     author="ISA Group",
     author_email="resinas@us.es",
     description="PPINOT for Python (ppinot4py)",
